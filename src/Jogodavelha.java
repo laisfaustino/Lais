35e8d852e19c05e9d05afc45879e5b3bcdfa4c72
Lais <laisfaustino@hotmail.com>
2017-03-02 22:41:47 -0300

    Projeto Java

---
+++ src/Jogodavelha.java
@@ -0,0 +1,14 @@
+
+public class Jogodavelha {
+    public static void main(String[] args){
+        System.out.println("_|_|x");
+        System.out.println("0|0|x");
+        System.out.println(" |0|x");
+     
+        
+    }
+        
+      
+
+    
+}
